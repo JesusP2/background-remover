814047160dd69af712610fda628a886614f42063
JesusP2 <jesus_perez99@hotmail.com>
2024-03-24 21:47:15 -0600

    working state

--- website/src/lib/utils.ts
+++ website/src/lib/utils.ts
@@ -1,9 +1,4 @@
-export function drawImage(
-	ctx: CanvasRenderingContext2D,
-	img: HTMLImageElement,
-) {
-	ctx.drawImage(img, 0, 0, img.width, img.height);
-}
+import { onMount } from "solid-js";
 
 export function getCanvas() {
 	const sourceCanvas = document.querySelector<HTMLCanvasElement>("#source");
@@ -17,42 +12,115 @@ export function getCanvas() {
 	return { sourceCtx, destinationCtx };
 }
 
-export function reDrawCanvas(
-	img: HTMLImageElement | null,
-	scale: number,
-	pos: { x: number; y: number },
-) {
-  if (!img) return;
-	const { sourceCtx, destinationCtx } = getCanvas();
-	sourceCtx.clearRect(
-		0,
-		0,
-		sourceCtx.canvas.width,
-		sourceCtx.canvas.height,
-	);
-	destinationCtx.clearRect(
-		0,
-		0,
-		destinationCtx.canvas.width,
-		destinationCtx.canvas.height,
-	);
-	sourceCtx.drawImage(img, pos.x, pos.y, img.width, img.height);
-	destinationCtx.drawImage(img, pos.x, pos.y, img.width, img.height);
-}
+export function useCanvas() {
+	const matrix = [1, 0, 0, 1, 0, 0];
+	let m = matrix;
+	let scale = 1;
+	const pos = { x: 0, y: 0 };
+	let dirty = true;
 
-export function updateCanvasScale(
-	newScale: number,
-) {
-	const { sourceCtx, destinationCtx } = getCanvas();
-	sourceCtx.restore();
-	sourceCtx.save();
-	destinationCtx.restore();
-	destinationCtx.save();
-	sourceCtx.scale(newScale, newScale);
-	destinationCtx.scale(newScale, newScale);
-}
+	const view = {
+		applyTo() {
+			if (dirty) {
+				this.update();
+			}
+			const { sourceCtx, destinationCtx } = getCanvas();
+			sourceCtx.clearRect(
+				0,
+				0,
+				sourceCtx.canvas.width,
+				sourceCtx.canvas.height,
+			);
+			sourceCtx.setTransform(m[0], m[1], m[2], m[3], m[4], m[5]);
+			sourceCtx.drawImage(sourceCtx.canvas, 0, 0);
+			destinationCtx.clearRect(
+				0,
+				0,
+				destinationCtx.canvas.width,
+				destinationCtx.canvas.height,
+			);
+			destinationCtx.setTransform(m[0], m[1], m[2], m[3], m[4], m[5]);
+			destinationCtx.drawImage(destinationCtx.canvas, 0, 0);
 
-export function getMousePos(e: MouseEvent, scale: number) {
-  console.log(e.clientX, e.clientY)
-	// console.log(e.clientX / scale, e.clientY / scale);
+			const zoomMe = document.querySelector("#zoomMe") as HTMLImageElement;
+			zoomMe.style.transform = `matrix(${m[0]},${m[1]},${m[2]},${m[3]},${m[4]},${m[5]})`;
+		},
+		update() {
+			dirty = false;
+			m[3] = m[0] = scale;
+			m[2] = m[1] = 0;
+			m[4] = pos.x;
+			m[5] = pos.y;
+		},
+		pan(amount: any) {
+			if (dirty) {
+				this.update();
+			}
+			pos.x += amount.x;
+			pos.y += amount.y;
+			dirty = true;
+		},
+		scaleAt(at: any, amount: any) {
+			// at in screen coords
+			if (dirty) {
+				this.update();
+			}
+			scale *= amount;
+			pos.x = at.x - (at.x - pos.x) * amount;
+			pos.y = at.y - (at.y - pos.y) * amount;
+			dirty = true;
+		},
+	};
+
+	const mouse = { x: 0, y: 0, oldX: 0, oldY: 0, button: false };
+	function mouseEvent(event: any) {
+		if (event.type === "mousedown") {
+			mouse.button = true;
+		}
+		if (event.type === "mouseup" || event.type === "mouseout") {
+			mouse.button = false;
+		}
+		mouse.oldX = mouse.x;
+		mouse.oldY = mouse.y;
+		mouse.x = event.pageX;
+		mouse.y = event.pageY;
+		if (mouse.button) {
+			view.pan({ x: mouse.x - mouse.oldX, y: mouse.y - mouse.oldY });
+			view.applyTo();
+		}
+		event.preventDefault();
+	}
+	function mouseWheelEvent(event: any) {
+		const zoomMe = document.querySelector("#zoomMe") as HTMLImageElement;
+    console.log(zoomMe.width, zoomMe.height)
+		const x = event.pageX - zoomMe.width / 2;
+		const y = event.pageY - zoomMe.height / 2;
+		if (event.deltaY < 0) {
+			view.scaleAt({ x, y }, 1.1);
+			view.applyTo();
+		} else {
+			view.scaleAt({ x, y }, 1 / 1.1);
+			view.applyTo();
+		}
+		event.preventDefault();
+	}
+	onMount(() => {
+  //   const { sourceCtx, destinationCtx } = getCanvas();
+  //   sourceCtx.canvas.addEventListener("mousemove", mouseEvent, { passive: false });
+  //   sourceCtx.canvas.addEventListener("mousedown", mouseEvent, { passive: false });
+  //   sourceCtx.canvas.addEventListener("mouseup", mouseEvent, { passive: false });
+  //   sourceCtx.canvas.addEventListener("mouseout", mouseEvent, { passive: false });
+		// sourceCtx.canvas.addEventListener("wheel", mouseWheelEvent, { passive: false });
+  //   destinationCtx.canvas.addEventListener("mousemove", mouseEvent, { passive: false });
+  //   destinationCtx.canvas.addEventListener("mousedown", mouseEvent, { passive: false });
+  //   destinationCtx.canvas.addEventListener("mouseup", mouseEvent, { passive: false });
+  //   destinationCtx.canvas.addEventListener("mouseout", mouseEvent, { passive: false });
+		// destinationCtx.canvas.addEventListener("wheel", mouseWheelEvent, { passive: false });
+
+		document.addEventListener("mousemove", mouseEvent, { passive: false });
+		document.addEventListener("mousedown", mouseEvent, { passive: false });
+		document.addEventListener("mouseup", mouseEvent, { passive: false });
+		document.addEventListener("mouseout", mouseEvent, { passive: false });
+		document.addEventListener("wheel", mouseWheelEvent, { passive: false });
+	});
 }
--- website/src/routes/index.tsx
+++ website/src/routes/index.tsx
@@ -1,71 +1,26 @@
 import { createEffect, createSignal, onMount } from "solid-js";
 import { ActionsMenu } from "../components/actions-menu";
-import {
-	getCanvas,
-	getMousePos,
-	reDrawCanvas,
-	updateCanvasScale,
-} from "~/lib/utils";
+import { useCanvas } from "~/lib/utils";
+// import {
+// 	getCanvas,
+// 	getMousePos,
+// 	reDrawCanvas,
+// 	updateCanvasScale,
+// } from "~/lib/utils";
 export default function Home() {
-	const [scale, setScale] = createSignal(1);
-	const [img, setImg] = createSignal<HTMLImageElement | null>(null);
-	const [translatePos, setTranslatePos] = createSignal({ x: 0, y: 0 });
-
-	onMount(() => {
-		const { sourceCtx, destinationCtx } = getCanvas();
-		sourceCtx.canvas.width = innerWidth / 2;
-		sourceCtx.canvas.height = innerHeight;
-		destinationCtx.canvas.width = innerWidth / 2;
-		destinationCtx.canvas.height = innerHeight;
-		sourceCtx.imageSmoothingEnabled = false;
-		destinationCtx.imageSmoothingEnabled = false;
-	});
-
+	useCanvas();
 	return (
-		<main class="flex">
-			<ActionsMenu
-				scale={scale}
-				setScale={setScale}
-				setImg={setImg}
-				translatePos={translatePos}
-				setTranslatePos={setTranslatePos}
+		<>
+			<main class="flex">
+				<canvas class="w-[49.95%] h-screen svg-bg" id="source"></canvas>
+				<div class="h-screen w-[0.1%] bg-zinc-400" />
+				<canvas class="w-[49.95%] h-screen svg-bg" id="destination"></canvas>
+			</main>
+			<img
+				id="zoomMe"
+				class="zoomables"
+				src="https://i.stack.imgur.com/C7qq2.png?s=328&g=1"
 			/>
-			<canvas
-				onWheel={(e) => {
-					if (e.deltaY > 0) {
-						const newScale = scale() - 0.17;
-						setScale(newScale);
-						updateCanvasScale(scale());
-						reDrawCanvas(img(), scale(), {
-							x: translatePos().x + e.clientX / scale() - e.clientX,
-							y: translatePos().y + e.clientY / scale() - e.clientY,
-						});
-					} else {
-						const newScale = scale() + 0.17;
-						const factor = newScale / 0.927;
-						setScale(newScale);
-						updateCanvasScale(scale());
-						// setTranslatePos({
-						// 	x: 0,
-						// 	y: translatePos().y + e.clientY / scale() - e.clientY,
-						// });
-						// setTranslatePos({
-						// 	x: 0,
-						// 	y:
-						// 		translatePos().y / factor - (e.clientY - translatePos().y) / scale(),
-						// });
-						reDrawCanvas(img(), scale(), {
-              x: 0,
-              y: (183.907 + 0) / factor - 0,
-            });
-					}
-				}}
-				onMouseMove={(e) => getMousePos(e, scale())}
-				class="w-[49.95%] h-screen border svg-bg"
-				id="source"
-			></canvas>
-			<div class="h-screen w-[0.1%] bg-zinc-400" />
-			<canvas class="w-[49.95%] h-screen svg-bg" id="destination"></canvas>
-		</main>
+		</>
 	);
 }
