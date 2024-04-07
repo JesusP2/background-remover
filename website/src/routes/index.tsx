0a3a27b2a31ae2d807c2e03459d71d4dec5a4d75
JesusP2 <jesus_perez99@hotmail.com>
2024-04-06 23:12:44 -0600

    removed uploadthing

--- website/src/routes/index.tsx
+++ website/src/routes/index.tsx
@@ -1,25 +1,14 @@
-import { useCanvas } from '~/lib/canvas';
-import { ActionsMenu } from '../components/actions-menu';
-
-export default function Home() {
-  const { onFileChange, setCurrentMode, applyMaskToImage, undo, redo, actions, redoActions } = useCanvas();
-
+export default function Index() {
   return (
-    <>
-      <main class="flex">
-        <ActionsMenu
-          onFileChange={onFileChange}
-          applyMaskToImage={applyMaskToImage}
-          setCurrentMode={setCurrentMode}
-          undo={undo}
-          redo={redo}
-          actions={actions}
-          redoActions={redoActions}
-        />
-        <canvas class="w-[49.95%] h-screen svg-bg" id="source" />
-        <div class="h-screen w-[0.1%] bg-zinc-400" />
-        <canvas class="w-[49.95%] h-screen svg-bg" id="destination" />
-      </main>
-    </>
+    <div>
+      <div class="grid place-items-center h-screen text-center">
+        <div>
+          <h1>Erased</h1>
+          <p>Remoove background / Edit images</p>
+        </div>
+      </div>
+      <div>
+      </div>
+    </div>
   );
 }
