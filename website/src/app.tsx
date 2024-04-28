2bc85360ae97d01a711f9e2a32b1fe636e354a73
JesusP2 <jesus_perez99@hotmail.com>
2024-04-28 00:32:18 -0600

    added font

--- website/src/app.tsx
+++ website/src/app.tsx
@@ -1,3 +1,5 @@
+import '@fontsource-variable/gabarito';
+import '@fontsource/geist-sans';
 import { Router } from '@solidjs/router';
 import { FileRoutes } from '@solidjs/start/router';
 import { Suspense } from 'solid-js';
