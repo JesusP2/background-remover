987526376c74577703c09c9836e52b26e2ae0707
JesusP2 <jesus_perez99@hotmail.com>
2024-04-30 18:24:46 -0600

    moved svgs to svg files

--- website/src/routes/my-gallery.tsx
+++ website/src/routes/my-gallery.tsx
@@ -57,24 +57,8 @@ export default function MyGallery() {
                   {image.name}
                 </div>
                 <div class="flex gap-x-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="15"
-                    height="15"
-                    fill="#000000"
-                    viewBox="0 0 256 256"
-                  >
-                    <path d="M224,144v64a8,8,0,0,1-8,8H40a8,8,0,0,1-8-8V144a8,8,0,0,1,16,0v56H208V144a8,8,0,0,1,16,0Zm-101.66,5.66a8,8,0,0,0,11.32,0l40-40a8,8,0,0,0-11.32-11.32L136,124.69V32a8,8,0,0,0-16,0v92.69L93.66,98.34a8,8,0,0,0-11.32,11.32Z"></path>
-                  </svg>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="15"
-                    height="15"
-                    fill="#000000"
-                    viewBox="0 0 256 256"
-                  >
-                    <path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path>
-                  </svg>
+                  <img src="/public/download-icon.svg" alt={`download-${image.name}`} />
+                  <img src="/public/x-icon.svg" alt={`delete-${image.name}`} />
                 </div>
               </div>
             </div>
