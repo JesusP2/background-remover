ab80399609ccad479b8e841393ba60d5d46dfc1f
JesusP2 <jesus_perez99@hotmail.com>
2024-09-07 02:08:24 -0600

    some testing stuff

--- backend/server.py
+++ backend/server.py
@@ -75,6 +75,15 @@ async def apply_mask_endpoint(
     base_mask_array[mask_array == 122] = cv2.GC_BGD
     base_mask_array[mask_array == 177] = cv2.GC_FGD
 
+    # base_mask_array = np.zeros(image.shape, np.uint8)
+    # base_mask_array = cv2.cvtColor(base_mask_array, cv2.COLOR_RGB2GRAY)
+    # # base_mask_array_copy = cv2.cvtColor(np.zeros(image.shape, np.uint8), cv2.COLOR_RGB2GRAY)
+    # base_mask_array[:] = cv2.GC_PR_BGD
+    # # base_mask_array[base_mask_array_copy == 0] = cv2.GC_PR_BGD
+    # # base_mask_array[base_mask_array_copy == 255] = cv2.GC_PR_FGD
+    # base_mask_array[mask_array == 122] = cv2.GC_BGD
+    # base_mask_array[mask_array == 177] = cv2.GC_FGD
+
     cv2.grabCut(
         image, base_mask_array, None, bgdModel, fgdModel, 1, cv2.GC_INIT_WITH_MASK
     )
