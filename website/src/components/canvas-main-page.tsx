b20e0ebff866f9699a565c7a340bd3f04d9acad5
JesusP2 <jesus_perez99@hotmail.com>
2024-12-15 19:17:27 -0600

    removed unused components

--- website/src/components/canvas-main-page.tsx
+++
@@ -1,9 +0,0 @@
-import { onMount } from "solid-js"
-import { urlToImage } from "~/hooks/use-grabcut-canvas/utils"
-
-export function MainPageCanvas() {
-  onMount(async () => {
-    const img = await urlToImage('/collage.png')
-  })
-  return (<canvas />)
-}
