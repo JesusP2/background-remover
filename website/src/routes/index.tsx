ff16082625ebf201a4f831c93a95b562042f7afc
JesusP2 <jesus_perez99@hotmail.com>
2024-12-21 23:44:44 -0600

    changes

--- website/src/routes/index.tsx
+++ website/src/routes/index.tsx
@@ -63,8 +63,47 @@ const CarouselDemo = () => {
               <CardContent class="flex aspect-square items-center justify-center p-6">
                 <CanvasAlphaMaskAnimation
                   canvasId="myCanvas3"
-                  url1="/img-without-bg-3.png"
-                  url2="/img-without-bg-3.jpg"
+                  url1="/img-without-bg-2.png"
+                  url2="/img-without-bg-2.jpeg"
+                />
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem>
+          <div class="p-1">
+            <Card>
+              <CardContent class="flex aspect-square items-center justify-center p-6">
+                <CanvasAlphaMaskAnimation
+                  canvasId="myCanvas4"
+                  url1="/img-without-bg-6.png"
+                  url2="/img-without-bg-6.jpg"
+                />
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem>
+          <div class="p-1">
+            <Card>
+              <CardContent class="flex aspect-square items-center justify-center p-6">
+                <CanvasAlphaMaskAnimation
+                  canvasId="myCanvas5"
+                  url1="/img-without-bg-7.png"
+                  url2="/img-without-bg-7.jpg"
+                />
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem>
+          <div class="p-1">
+            <Card>
+              <CardContent class="flex aspect-square items-center justify-center p-6">
+                <CanvasAlphaMaskAnimation
+                  canvasId="myCanvas6"
+                  url1="/img-without-bg-8.png"
+                  url2="/img-without-bg-8.jpeg"
                 />
               </CardContent>
             </Card>
@@ -82,10 +121,24 @@ function GetStartedButton(props: any) {
   return (
     <button class="relative h-12" {...props}>
       <div class="border-2 border-black bg-white rounded-lg w-36 h-12 relative">
-        <div class="bg-black w-36 h-12 relative top-[-0.7rem] rounded-lg text-white grid place-items-center font-gabarito text-lg group-hover:top-[-0.4rem] group-hover:left-[0.2rem] left-[0.5rem] duration-100">
+        <div class="bg-black w-36 h-12 relative top-[-0.7rem] rounded-lg text-white grid place-items-center font-gabarito text-lg group-hover:top-[-0.4rem] group-hover:left-[0.2rem] left-[0.5rem] duration-200">
           Get Started
         </div>
       </div>
+    </button>
+  );
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+function GetStartedButton2(props: any) {
+  return (
+    <button class="group w-56 relative" {...props}>
+      <div class="-skew-y-[45deg] group-hover:skew-y-[45deg] duration-300 w-[9px] h-10 bg-white border-2 border-black absolute -left-[7px] top-[4px] group-hover:top-[12px] bg-white z-50" />
+      <div class="h-10 w-full grid place-items-center border-2 border-black font-bold relative top-0 group-hover:top-4 ease-in-out duration-300 bg-white z-50">
+        Start your free trial
+      </div>
+      <div class="-skew-x-[45deg] h-2 w-full border-2 border-t-0 border-black relative right-[4px]" />
+      <div class="skew-x-[45deg] h-2 w-full border-2 border-b-0 border-black absolute right-[4px] top-2" />
     </button>
   );
 }
@@ -157,7 +210,7 @@ export default function Page() {
                   setEditorSelected(false);
                 }}
               >
-                <DialogTrigger as={GetStartedButton} />
+                <DialogTrigger as={GetStartedButton2} />
                 <DialogContentWithoutClose>
                   <Show when={!editorSelected()}>
                     <A
@@ -181,7 +234,7 @@ export default function Page() {
             </Match>
             <Match when={displayOn() === 'drawer'}>
               <Drawer>
-                <DrawerTrigger as={GetStartedButton} variant="outline" />
+                <DrawerTrigger as={GetStartedButton2} variant="outline" />
                 <DrawerContent>
                   <div class="mx-auto w-full max-w-sm mb-8">
                     <div class="p-4 pb-0 flex flex-col gap-y-2">
@@ -215,7 +268,6 @@ export default function Page() {
             </Match>
           </Switch>
         </div>
-        <CarouselDemo />
       </main>
     </div>
   );
